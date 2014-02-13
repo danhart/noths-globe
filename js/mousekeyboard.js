8e183e59685931768f8b459e36a2ae5101b449a4
Dan Hart <danhart@notonthehighstreet.com>
2014-02-13 20:42:08 +0000

    Tidy up

--- js/mousekeyboard.js
+++ js/mousekeyboard.js
@@ -48,32 +48,6 @@ function onDocumentMouseUp( event ){
 function onClick( event ){
     // Not using this for now.
     return true;
-
-    //  make the rest not work if the event was actually a drag style click
-    if( Math.abs(pressX - mouseX) > 3 || Math.abs(pressY - mouseY) > 3 )
-        return;
-
-    var pickColorIndex = getPickColor();
-    //  find it
-    for( var i in countryColorMap ){
-        var countryCode = i;
-        var countryColorIndex = countryColorMap[i];
-        if( pickColorIndex == countryColorIndex ){
-            // console.log("selecting code " + countryCode);
-            var countryName = countryLookup[countryCode];
-            // console.log("converts to " + countryName);
-            if( countryName === undefined )
-                return;
-            if( $.inArray(countryName, selectableCountries) <= -1 )
-                return;
-            // console.log(countryName);
-            var selection = selectionData;
-            selection.selectedCountry = countryName;
-            selectVisualization( timeBins, selection.selectedYear, [selection.selectedCountry], selection.getExportCategories(), selection.getImportCategories() );
-            // console.log('selecting ' + countryName + ' from click');
-            return;
-        }
-    }
 }
 
 function onKeyDown( event ){
@@ -88,15 +62,15 @@ function onMouseWheel( event ){
     var delta = 0;
 
     if (event.wheelDelta) { /* IE/Opera. */
-            delta = event.wheelDelta/120;
+        delta = event.wheelDelta/120;
     }
+
     //  firefox
     else if( event.detail ){
         delta = -event.detail/3;
     }
 
-    if (delta)
-            handleMWheel(delta);
+    if (delta) handleMWheel(delta);
 
     event.returnValue = false;
 }
