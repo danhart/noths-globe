2cad67582a296bfd07ec79867cf2ee49aba69346
Dan Hart <danhart@notonthehighstreet.com>
2014-02-19 13:00:27 +0000

    Separate order creation from globe setup

--- js/noths_main.js
+++ js/noths_main.js
@@ -148,25 +148,31 @@
         async.series(createPathPointers, callback);
     };
 
+    var socket = io.connect('http://85.17.23.72:10052', {
+        resource: 'noths_order_geo/socket.io'
+    });
+
+    // TODO: Move this state into the GlobePaths plugin and add an event for
+    // Globe Ready.
+    var globeStarted = false;
+
     GlobePaths.start(function() {
-        var socket = io.connect('http://85.17.23.72:10052', {
-            resource: 'noths_order_geo/socket.io'
+        globeStarted = true;
+    });
+
+    socket.on('intl_orders', function(ordersData) {
+        var orders = ordersData.map(function(orderData) {
+            return new Order(orderData);
         });
 
-        socket.on('intl_orders', function(ordersData) {
-            var orders = ordersData.map(function(orderData) {
-                return new Order(orderData);
+        var orderCollection = new OrderCollection(orders);
+
+        orderCollection.createPaths(function(err, paths) {
+            paths.forEach(function(path) {
+                if (path) pathCollection.push(path);
             });
 
-            var orderCollection = new OrderCollection(orders);
-
-            orderCollection.createPaths(function(err, paths) {
-                paths.forEach(function(path) {
-                    if (path) pathCollection.push(path);
-                });
-
-                GlobePaths.setPaths(pathCollection.getData());
-            });
+            if (globeStarted) GlobePaths.setPaths(pathCollection.getData());
         });
     });
 })();
