ded1fb8be7761ff7dae00371dfbf200e72824db7
Dan Hart <danhart@notonthehighstreet.com>
2014-02-23 23:17:25 +0000

    Update path appearance

--- js/path.js
+++ js/path.js
@@ -5,10 +5,17 @@ define(function() {
 
     var PATH_COLORS = [
         0xdd380c, // Red
+        0xdd0c0c, // Blood red
+        0xa21111, // Blood red darker
         0x3dba00, // Green
+        0x00ba65, // Mint green
         0x154492, // Blue
+        0x158292, // Turquoise
         0xe011cf, // Purple
-        0xe29d08  // Orange
+        0x9b0e8f, // Purple darker
+        0xe29d08, // Orange
+        0xa4740c, // Orange darker
+        0xbfb40a  // Yellow
     ];
 
     var Path = function() {
@@ -17,11 +24,11 @@ define(function() {
     };
 
     Path.prototype.randomParticleCount = function() {
-        this.particleCount = randBetweenRange(5, 100);
+        this.particleCount = randBetweenRange(5, 150);
     };
 
     Path.prototype.randomParticleSize = function() {
-        this.particleSize = randBetweenRange(5, 50);
+        this.particleSize = randBetweenRange(10, 40);
     };
 
     Path.prototype.randomColor = function() {
